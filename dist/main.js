c978edc781fa2ace2319138fec6674d7b66ffd80
Alex Galhardo <alex.vieira@pravaler.com.br>
2023-09-08 20:25:24 -0300

    fix: detalhes

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(App_module_1.AppModule);
     app.enableCors();
     await app.listen(process.env.PORT || 3000, () => {
-        console.log(`\n\nGalhardo MicroSaaS API running on http://localhost:${process.env.PORT || 3000}`);
+        console.log(`\n\nAPI.NERDAPI.COM running on http://localhost:${process.env.PORT || 3000}`);
     });
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     await app.listen(process.env.PORT || 3000, () => {
-        console.log(`\n\nGalhardo MicroSaaS API running on http://localhost:${process.env.PORT || 3000}`);
+        console.log(`\n\nAPI.NERDAPI.COM running on http://localhost:${process.env.PORT || 3000}`);
     });
 }
 
