53e49766f9217284f7abc8cc2896ac1f46572236
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-17 10:54:53 -0300

    fix: details

--- src/Controllers/Stripe.controller.ts
+++ src/Controllers/Stripe.controller.ts
@@ -3,8 +3,8 @@ import { ApiBearerAuth, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Response } from "express";
 import { StripeRepositoryPort } from "src/Repositories/Stripe.repository";
 import {
-	StripeCreateCheckoutSessionDTO,
-	StripeCreateCheckoutSessionUseCasePort,
+    StripeCreateCheckoutSessionDTO,
+    StripeCreateCheckoutSessionUseCasePort,
 } from "src/UseCases/StripeCreateCheckoutSession.useCase";
 import { StripeCreatePortalSessionUseCasePort } from "src/UseCases/StripeCreatePortalSession.useCase";
 import { StripeWebhookChargeSucceededUseCasePort } from "src/UseCases/StripeWebhookChargeSucceeded.useCase";
@@ -12,175 +12,175 @@ import { StripeWebhookInvoiceFinalizedUseCasePort } from "src/UseCases/StripeWeb
 import TelegramBOTLogger from "src/Utils/TelegramBOTLogger";
 
 interface StripeUseCaseResponse {
-	success: boolean;
-	redirect?: string;
-	message?: string;
+    success: boolean;
+    redirect?: string;
+    message?: string;
 }
 
 interface StripeWebhookEventDTO {
-	id: string;
-	type: string;
-	event: any;
-	data: any;
-	created: number;
+    id: string;
+    type: string;
+    event: any;
+    data: any;
+    created: number;
 }
 
 interface StripeCreatePortalSessionDTO {
-	session_id: string;
+    session_id: string;
 }
 
 interface StripeControllerPort {
-	createCheckoutSession(
-		stripeCreateCheckoutSessionDTO: StripeCreateCheckoutSessionDTO,
-		response: Response,
-	): Promise<Response<StripeUseCaseResponse>>;
-	createPortalSession(
-		stripeCreatePortalSessionDTO: StripeCreatePortalSessionDTO,
-		response: Response,
-	): Promise<Response<StripeUseCaseResponse>>;
-	webhook(event: StripeWebhookEventDTO, response: Response): Promise<Response<{ received: boolean }>>;
+    createCheckoutSession(
+        stripeCreateCheckoutSessionDTO: StripeCreateCheckoutSessionDTO,
+        response: Response,
+    ): Promise<Response<StripeUseCaseResponse>>;
+    createPortalSession(
+        stripeCreatePortalSessionDTO: StripeCreatePortalSessionDTO,
+        response: Response,
+    ): Promise<Response<StripeUseCaseResponse>>;
+    webhook(event: StripeWebhookEventDTO, response: Response): Promise<Response<{ received: boolean }>>;
 }
 
 @Controller("stripe")
 @ApiTags("stripe")
 export class StripeController implements StripeControllerPort {
-	constructor(
-		@Inject("StripeRepositoryPort") private readonly stripeRepository: StripeRepositoryPort,
-		@Inject("StripeCreateCheckoutSessionUseCasePort")
-		private readonly stripeCreateCheckoutSessionUseCase: StripeCreateCheckoutSessionUseCasePort,
-		@Inject("StripeCreatePortalSessionUseCasePort")
-		private readonly stripeCreatePortalSessionUseCase: StripeCreatePortalSessionUseCasePort,
-		@Inject("StripeWebhookChargeSucceededUseCasePort")
-		private readonly stripeWebhookChargeSucceededUseCase: StripeWebhookChargeSucceededUseCasePort,
-		@Inject("StripeWebhookInvoiceFinalizedUseCasePort")
-		private readonly stripeWebhookInvoiceFinalizedUseCase: StripeWebhookInvoiceFinalizedUseCasePort,
-	) { }
+    constructor(
+        @Inject("StripeRepositoryPort") private readonly stripeRepository: StripeRepositoryPort,
+        @Inject("StripeCreateCheckoutSessionUseCasePort")
+        private readonly stripeCreateCheckoutSessionUseCase: StripeCreateCheckoutSessionUseCasePort,
+        @Inject("StripeCreatePortalSessionUseCasePort")
+        private readonly stripeCreatePortalSessionUseCase: StripeCreatePortalSessionUseCasePort,
+        @Inject("StripeWebhookChargeSucceededUseCasePort")
+        private readonly stripeWebhookChargeSucceededUseCase: StripeWebhookChargeSucceededUseCasePort,
+        @Inject("StripeWebhookInvoiceFinalizedUseCasePort")
+        private readonly stripeWebhookInvoiceFinalizedUseCase: StripeWebhookInvoiceFinalizedUseCasePort,
+    ) {}
 
-	@Post("/create-checkout-session")
-	@ApiBearerAuth()
-	async createCheckoutSession(
-		@Body() stripeCreateCheckoutSessionDTO: StripeCreateCheckoutSessionDTO,
-		@Res() response: Response,
-	): Promise<Response<StripeUseCaseResponse>> {
-		try {
-			const userJWTToken = response.locals.token;
-			const { success, redirect } = await this.stripeCreateCheckoutSessionUseCase.execute(
-				userJWTToken,
-				stripeCreateCheckoutSessionDTO,
-			);
-			if (success) return response.status(HttpStatus.OK).json({ success: true, redirect });
-		} catch (error) {
-			return response.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
-		}
-	}
+    @Post("/create-checkout-session")
+    @ApiBearerAuth()
+    async createCheckoutSession(
+        @Body() stripeCreateCheckoutSessionDTO: StripeCreateCheckoutSessionDTO,
+        @Res() response: Response,
+    ): Promise<Response<StripeUseCaseResponse>> {
+        try {
+            const userJWTToken = response.locals.token;
+            const { success, redirect } = await this.stripeCreateCheckoutSessionUseCase.execute(
+                userJWTToken,
+                stripeCreateCheckoutSessionDTO,
+            );
+            if (success) return response.status(HttpStatus.OK).json({ success: true, redirect });
+        } catch (error) {
+            return response.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
+        }
+    }
 
-	@Post("/create-portal-session")
-	@ApiBearerAuth()
-	async createPortalSession(
-		@Body() stripeCreatePortalSessionDTO: StripeCreatePortalSessionDTO,
-		@Res() response: Response,
-	): Promise<Response<StripeUseCaseResponse>> {
-		try {
-			const userJWTToken = response.locals.token;
-			const { success, redirect } = await this.stripeCreatePortalSessionUseCase.execute(
-				userJWTToken,
-				stripeCreatePortalSessionDTO,
-			);
-			if (success) return response.status(HttpStatus.OK).json({ success: true, redirect });
-		} catch (error) {
-			return response.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
-		}
-	}
+    @Post("/create-portal-session")
+    @ApiBearerAuth()
+    async createPortalSession(
+        @Body() stripeCreatePortalSessionDTO: StripeCreatePortalSessionDTO,
+        @Res() response: Response,
+    ): Promise<Response<StripeUseCaseResponse>> {
+        try {
+            const userJWTToken = response.locals.token;
+            const { success, redirect } = await this.stripeCreatePortalSessionUseCase.execute(
+                userJWTToken,
+                stripeCreatePortalSessionDTO,
+            );
+            if (success) return response.status(HttpStatus.OK).json({ success: true, redirect });
+        } catch (error) {
+            return response.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
+        }
+    }
 
-	// https://github.com/stripe/stripe-cli
-	// https://dashboard.stripe.com/webhooks
-	@Post("/webhook")
-	@ApiResponse({ status: 200 })
-	async webhook(
-		@Body() event: StripeWebhookEventDTO,
-		@Res() response: Response,
-	): Promise<Response<{ received: boolean }>> {
-		try {
-			switch (event.type) {
-				case "payment_intent.succeeded":
-					this.stripeRepository.savePaymentWebhookEventLog(event);
-					break;
+    // https://github.com/stripe/stripe-cli
+    // https://dashboard.stripe.com/webhooks
+    @Post("/webhook")
+    @ApiResponse({ status: 200 })
+    async webhook(
+        @Body() event: StripeWebhookEventDTO,
+        @Res() response: Response,
+    ): Promise<Response<{ received: boolean }>> {
+        try {
+            switch (event.type) {
+                case "payment_intent.succeeded":
+                    this.stripeRepository.savePaymentWebhookEventLog(event);
+                    break;
 
-				case "invoice.paid":
-					this.stripeRepository.saveInvoiceWebhookEventLog(event);
-					break;
+                case "invoice.paid":
+                    this.stripeRepository.saveInvoiceWebhookEventLog(event);
+                    break;
 
-				case "charge.succeeded":
-					this.stripeWebhookChargeSucceededUseCase.execute(event);
-					break;
+                case "charge.succeeded":
+                    this.stripeWebhookChargeSucceededUseCase.execute(event);
+                    break;
 
-				case "invoice.finalized":
-					this.stripeWebhookInvoiceFinalizedUseCase.execute(event);
-					break;
+                case "invoice.finalized":
+                    this.stripeWebhookInvoiceFinalizedUseCase.execute(event);
+                    break;
 
-				case "customer.created":
-					this.stripeRepository.saveCustomerWebhookEventLog(event);
-					break;
+                case "customer.created":
+                    this.stripeRepository.saveCustomerWebhookEventLog(event);
+                    break;
 
-				case "customer.subscription.updated":
-					this.stripeRepository.saveCustomerWebhookEventLog(event);
-					break;
+                case "customer.subscription.updated":
+                    this.stripeRepository.saveCustomerWebhookEventLog(event);
+                    break;
 
-				case "invoice.updated":
-					this.stripeRepository.saveInvoiceWebhookEventLog(event);
-					break;
+                case "invoice.updated":
+                    this.stripeRepository.saveInvoiceWebhookEventLog(event);
+                    break;
 
-				case "customer.updated":
-					this.stripeRepository.saveCustomerWebhookEventLog(event);
-					break;
+                case "customer.updated":
+                    this.stripeRepository.saveCustomerWebhookEventLog(event);
+                    break;
 
-				case "invoice.payment_succeeded":
-					this.stripeRepository.saveInvoiceWebhookEventLog(event);
-					break;
+                case "invoice.payment_succeeded":
+                    this.stripeRepository.saveInvoiceWebhookEventLog(event);
+                    break;
 
-				case "customer.subscription.created":
-					this.stripeRepository.saveCustomerWebhookEventLog(event);
-					break;
+                case "customer.subscription.created":
+                    this.stripeRepository.saveCustomerWebhookEventLog(event);
+                    break;
 
-				case "invoice.created":
-					this.stripeRepository.saveInvoiceWebhookEventLog(event);
-					break;
+                case "invoice.created":
+                    this.stripeRepository.saveInvoiceWebhookEventLog(event);
+                    break;
 
-				case "payment_method.attached":
-					this.stripeRepository.savePaymentWebhookEventLog(event);
-					break;
+                case "payment_method.attached":
+                    this.stripeRepository.savePaymentWebhookEventLog(event);
+                    break;
 
-				case "payment_intent.created":
-					this.stripeRepository.savePaymentWebhookEventLog(event);
-					break;
+                case "payment_intent.created":
+                    this.stripeRepository.savePaymentWebhookEventLog(event);
+                    break;
 
-				case "checkout.session.completed":
-					this.stripeRepository.saveCheckoutSessionWebhookEventLog(event);
-					break;
+                case "checkout.session.completed":
+                    this.stripeRepository.saveCheckoutSessionWebhookEventLog(event);
+                    break;
 
-				case "checkout.session.expired":
-					this.stripeRepository.saveCheckoutSessionWebhookEventLog(event);
-					break;
+                case "checkout.session.expired":
+                    this.stripeRepository.saveCheckoutSessionWebhookEventLog(event);
+                    break;
 
-				case "balance.available":
-					this.stripeRepository.savePaymentWebhookEventLog(event);
-					break;
+                case "balance.available":
+                    this.stripeRepository.savePaymentWebhookEventLog(event);
+                    break;
 
-				case "billing_portal.session.created":
-					this.stripeRepository.saveBillingPortalSessionWebhookEventLog(event);
-					break;
+                case "billing_portal.session.created":
+                    this.stripeRepository.saveBillingPortalSessionWebhookEventLog(event);
+                    break;
 
-				case "account.updated":
-					this.stripeRepository.saveCustomerWebhookEventLog(event);
-					break;
+                case "account.updated":
+                    this.stripeRepository.saveCustomerWebhookEventLog(event);
+                    break;
 
-				default:
-					TelegramBOTLogger.logError(event.type);
-			}
+                default:
+                    TelegramBOTLogger.logError(event.type);
+            }
 
-			return response.json({ received: true });
-		} catch (error) {
-			return response.json({ received: false });
-		}
-	}
+            return response.json({ received: true });
+        } catch (error) {
+            return response.json({ received: false });
+        }
+    }
 }
