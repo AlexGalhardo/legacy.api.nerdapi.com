b92706b52992c6ae7a8a9074a364fe6b6faafcd0
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-22 11:45:33 -0300

    fix: details in tsconfig to build telegram-logger

--- src/use-cases/stripe-webhook-invoice-finalized.use-case.ts
+++ src/use-cases/stripe-webhook-invoice-finalized.use-case.ts
@@ -3,54 +3,53 @@ import { StripeRepositoryPort } from "src/repositories/stripe.repository";
 import { UsersRepositoryPort } from "src/repositories/users.repository";
 import { ClientException } from "src/utils/exceptions.util";
 import { ErrorsMessages } from "src/utils/errors-messages.util";
-import TelegramLog from "src/config/telegram-logger.config";
 
 export interface StripeWebhookInvoiceFinalizedUseCasePort {
-    execute(event: any): void;
+	execute(event: any): void;
 }
 
 export default class StripeWebhookInvoiceFinalizedUseCase implements StripeWebhookInvoiceFinalizedUseCasePort {
-    constructor(
-        private readonly stripeRepository: StripeRepositoryPort,
-        private readonly usersRepository: UsersRepositoryPort,
-    ) {}
+	constructor(
+		private readonly stripeRepository: StripeRepositoryPort,
+		private readonly usersRepository: UsersRepositoryPort,
+	) { }
 
-    async execute(event: any) {
-        const { user } = await this.usersRepository.getByEmail(event.data.object.customer_email);
+	async execute(event: any) {
+		const { user } = await this.usersRepository.getByEmail(event.data.object.customer_email);
 
-        if (user) {
-            const userUpdated = await this.usersRepository.updateStripeSubscriptionInfo(user, {
-                customerId: event.data.object.customer ?? null,
-                invoiceId: event.data.object.id ?? null,
-                hostedInvoiceUrl: event.data.object.hosted_invoice_url ?? null,
-                amount: event.data.object.amount_due,
-                startAt: DateTime.timestampToGetNow(event.data.object.lines.data[0].period.start) ?? null,
-                endsAt: DateTime.timestampToGetNow(event.data.object.lines.data[0].period.end) ?? null,
-                createdAt: String(new Date(event.created)),
-                createdAtBrazil: DateTime.timestampToGetNow(event.created) ?? null,
-            });
+		if (user) {
+			const userUpdated = await this.usersRepository.updateStripeSubscriptionInfo(user, {
+				customerId: event.data.object.customer ?? null,
+				invoiceId: event.data.object.id ?? null,
+				hostedInvoiceUrl: event.data.object.hosted_invoice_url ?? null,
+				amount: event.data.object.amount_due,
+				startAt: DateTime.timestampToGetNow(event.data.object.lines.data[0].period.start) ?? null,
+				endsAt: DateTime.timestampToGetNow(event.data.object.lines.data[0].period.end) ?? null,
+				createdAt: String(new Date(event.created)),
+				createdAtBrazil: DateTime.timestampToGetNow(event.created) ?? null,
+			});
 
-            this.stripeRepository.saveInvoiceWebhookEventLog(event);
+			this.stripeRepository.saveInvoiceWebhookEventLog(event);
 
-            TelegramLog.info(`\n
-				<b>STRIPE CHARGE ID:</b> ${userUpdated.stripe.subscription.charge_id}
-				<b>STRIPE CHARGE PAID:</b> ${userUpdated.stripe.subscription.active}
-				<b>STRIPE RECEIPT URL:</b> ${userUpdated.stripe.subscription.receipt_url}
-				<b>STRIPE INVOICE URL:</b> ${userUpdated.stripe.subscription.hosted_invoice_url}
-				---------------------------------------------
-				<b>SUBSCRIPTION NAME:</b> ${userUpdated.stripe.subscription.name}
-				<b>SUBSCRIPTION AMOUNT:</b> ${userUpdated.stripe.subscription.name === "CASUAL" ? 199 : 499}
-				<b>SUBSCRIPTION START AT:</b> ${userUpdated.stripe.subscription.starts_at}
-				<b>SUBSCRIPTION ENDS AT:</b> ${userUpdated.stripe.subscription.ends_at}
-				---------------------------------------------
-				<b>STRIPE CUSTOMER ID:</b> ${userUpdated.stripe.customer_id}
-				<b>CUSTOMER NAME:</b> ${userUpdated.username}
-				<b>CUSTOMER EMAIL:</b> ${userUpdated.email}
-				<b>CUSTOMER SUBSCRIPTION ACTIVE: </b> ${userUpdated.stripe.subscription.active}
-				<b>CUSTOMER API TOKEN: </b> ${userUpdated.api_key}
-				`);
-        } else {
-            throw new ClientException(ErrorsMessages.USER_NOT_FOUND);
-        }
-    }
+			// TelegramLog.info(`\n
+			// 	<b>STRIPE CHARGE ID:</b> ${userUpdated.stripe.subscription.charge_id}
+			// 	<b>STRIPE CHARGE PAID:</b> ${userUpdated.stripe.subscription.active}
+			// 	<b>STRIPE RECEIPT URL:</b> ${userUpdated.stripe.subscription.receipt_url}
+			// 	<b>STRIPE INVOICE URL:</b> ${userUpdated.stripe.subscription.hosted_invoice_url}
+			// 	---------------------------------------------
+			// 	<b>SUBSCRIPTION NAME:</b> ${userUpdated.stripe.subscription.name}
+			// 	<b>SUBSCRIPTION AMOUNT:</b> ${userUpdated.stripe.subscription.name === "CASUAL" ? 199 : 499}
+			// 	<b>SUBSCRIPTION START AT:</b> ${userUpdated.stripe.subscription.starts_at}
+			// 	<b>SUBSCRIPTION ENDS AT:</b> ${userUpdated.stripe.subscription.ends_at}
+			// 	---------------------------------------------
+			// 	<b>STRIPE CUSTOMER ID:</b> ${userUpdated.stripe.customer_id}
+			// 	<b>CUSTOMER NAME:</b> ${userUpdated.username}
+			// 	<b>CUSTOMER EMAIL:</b> ${userUpdated.email}
+			// 	<b>CUSTOMER SUBSCRIPTION ACTIVE: </b> ${userUpdated.stripe.subscription.active}
+			// 	<b>CUSTOMER API TOKEN: </b> ${userUpdated.api_key}
+			// 	`);
+		} else {
+			throw new ClientException(ErrorsMessages.USER_NOT_FOUND);
+		}
+	}
 }
